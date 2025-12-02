d8987f14cf91930a7ae420c2bcd717a54829804e
agus-darmawan <wayanagus.dr@gmail.com>
2025-12-03 03:41:47 +0700

    add host

--- app.py
+++ app.py
@@ -759,4 +759,4 @@ def download_file(filename):
     return "File not found", 404
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True,host="0.0.0.0", port=5001)
